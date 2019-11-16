3a0f3c5c372ae57ce9d15e6345a5a20b73c0987b
screwdriver555 <56878093+screwdriver555@users.noreply.github.com>
2019-11-16 19:21:05 +0800

    上傳"最新消息"部分進度
    此項功能實作大致上完成，剩下部分細節待調整

---
+++ app/src/main/java/com/example/fivegroup/newsDetail_Activity.java
@@ -0,0 +1,38 @@
+package com.example.fivegroup;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class newsDetail_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newsdetail);
+
+        //加入自訂的ToolBar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("食藥新聞");
+
+        setSupportActionBar(toolbar);
+
+        TextView title = findViewById(R.id.dTitle);
+        TextView context = findViewById(R.id.context);
+
+        Bundle bundle = this.getIntent().getExtras();
+
+        if (bundle != null) {
+
+            String str1 = bundle.getString("title");
+            String str2 = bundle.getString("detail");
+
+            title.setText(str1);
+            context.setText(str2);
+        }
+
+    }
+
+}
